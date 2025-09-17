59345986721166f278698303af1718c299f9091e
eblankenship27 <blankenship.e@northeastern.edu>
2025-09-17 02:34:43 -0400

    Added name to labs page

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,9 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <p><strong>Full Name:</strong> Evan Blankenship</p>
+     <p><strong>Section Number:</strong> CS4550.11597.202610</p>
+     <p><strong>Link to Github repo: </strong><a href="https://github.com/eblankenship27/kambaz-next-js" target="_blank">Links to all relevant source code repositories</a></p>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
