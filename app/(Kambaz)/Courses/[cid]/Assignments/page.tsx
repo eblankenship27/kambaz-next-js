6b046d9e75bf6af1128ec2fcfd0e4baf876411e1
eblankenship27 <blankenship.e@northeastern.edu>
2025-09-17 02:14:58 -0400

    Changing a to Link for deployment

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,24 +11,24 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-          </a>
+          </Link>
           <br/>
           Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am | <strong>Due</strong> May 13 at 11:59pm | 100 pts
         </li>
         <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/2" className="wd-assignment-link" >
+            <Link href="/Courses/1234/Assignments/2" className="wd-assignment-link" >
                 A2 - CSS + BOOTSTRAP
-            </a>
+            </Link>
             <br/>
             Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am | <strong>Due</strong> May 20 at 11:59pm | 100 pts
         </li>
         <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/3" className="wd-assignment-link" >
+            <Link href="/Courses/1234/Assignments/3" className="wd-assignment-link" >
                 A3 - JAVASCRIPT + REACT
-            </a>
+            </Link>
             <br/>
             Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am | <strong>Due</strong> May 27 at 11:59pm | 100 pts
         </li>
