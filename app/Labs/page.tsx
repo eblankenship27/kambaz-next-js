b49c4fe70f8d8382793f7282a325693e595ba3c0
eblankenship27 <blankenship.e@northeastern.edu>
2025-11-15 02:02:06 -0500

    a5 Server

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -6,6 +6,8 @@ export default function Labs() {
      <p><strong>Full Name:</strong> Evan Blankenship</p>
      <p><strong>Section Number:</strong> CS4550.11597.202610</p>
      <p><strong>Link to Github repo: </strong><a href="https://github.com/eblankenship27/kambaz-next-js" target="_blank">Links to all relevant source code repositories</a></p>
+     <p><strong>Link to Server Github repo:</strong><a href="https://github.com/eblankenship27/kambaz-node-server-app" target="_blank">Link to Server GitHub repo</a></p>
+     <p><strong>Link to server base url:</strong><a href="https://kambaz-node-server-app-0tw3.onrender.com/" target="_blank">Link to server</a></p>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
